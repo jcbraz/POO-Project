3ba9d383763d121657e1646d537c1add60854f31
sica2602 <sjscosta02@gmail.com>
2022-05-09 15:34:42 +0100

    Adicionada documentação do ficheiro Fatura

--- src/Fatura.java
+++ src/Fatura.java
@@ -2,6 +2,7 @@ import java.time.LocalDate;
 
 public class Fatura implements Comparable<Fatura> {
 
+    // Declaring the variables that will be used in the class.
     private static int counter = 0;
     private LocalDate dataInicio;
     private LocalDate dataFim;
@@ -12,6 +13,7 @@ public class Fatura implements Comparable<Fatura> {
     private int codigoCasa;
     private int code;
 
+    // The default constructor.
     public Fatura() {
         this.dataInicio = LocalDate.now();
         this.dataFim = LocalDate.now();
@@ -23,6 +25,7 @@ public class Fatura implements Comparable<Fatura> {
         this.code = 0;
     }
 
+    // A constructor that receives the parameters and assigns them to the variables.
     public Fatura(LocalDate dateinicio, LocalDate datefim, int codefornecedor, int nifCliente, double consumo,
             double custo, int codigoCasa) {
         this.dataInicio = dateinicio;
@@ -35,74 +38,167 @@ public class Fatura implements Comparable<Fatura> {
         this.code = counter++;
     }
 
+    /**
+     * > This function returns the date of the beginning of the project
+     * 
+     * @return The date of the beginning of the project.
+     */
     public LocalDate getDateInicio() {
         return this.dataInicio;
     }
 
+    /**
+     * This function returns the end date of the event
+     * 
+     * @return The end date of the period.
+     */
     public LocalDate getDateFim() {
         return this.dataFim;
     }
 
+    /**
+     * This function returns the value of the variable codefornecedor
+     * 
+     * @return The method getFornecedor is returning the value of the variable codefornecedor.
+     */
     public int getFornecedor() {
         return this.codefornecedor;
     }
 
+    /**
+     * > This function returns the nifCliente of the object
+     * 
+     * @return The nifCliente variable.
+     */
     public int getNifCliente() {
         return this.nifCliente;
     }
 
+    /**
+     * This function returns the value of the variable "consumo"
+     * 
+     * @return The consumption of the vehicle.
+     */
     public double getConsumo() {
         return this.consumo;
     }
 
+    /**
+     * This function returns the value of the variable custo
+     * 
+     * @return The cost of the item.
+     */
     public double getCusto() {
         return this.custo;
     }
 
+    /**
+     * > This function returns the value of the variable codigoCasa
+     * 
+     * @return The method is returning the value of the variable codigoCasa.
+     */
     public int getCodigoCasa() {
         return this.codigoCasa;
     }
 
+    /**
+     * > This function returns the code of the current object
+     * 
+     * @return The code of the enum.
+     */
     public int getCode() {
         return this.code;
     }
 
+    /**
+     * It sets the dateInicio variable to the value of the dateInicio parameter
+     * 
+     * @param dateInicio The date that the user wants to start the search.
+     */
     public void setDateInicio(LocalDate dateInicio) {
         this.dataInicio = dateInicio;
     }
 
+    /**
+     * The function setDateFim() sets the dateFim variable to the value of the parameter dateFim
+     * 
+     * @param dateFim The end date of the period.
+     */
     public void setDateFim(LocalDate dateFim) {
         this.dataFim = dateFim;
     }
 
+    /**
+     * This function sets the value of the variable codefornecedor to the value of the variable
+     * codefornecedor
+     * 
+     * @param codefornecedor The code of the supplier
+     */
     public void setFornecedor(int codefornecedor) {
         this.codefornecedor = codefornecedor;
     }
 
+    /**
+     * This function sets the nifCliente variable to the value of the parameter nifCliente
+     * 
+     * @param nifCliente The client's NIF.
+     */
     public void setNifCliente(int nifCliente) {
         this.nifCliente = nifCliente;
     }
 
+    /**
+     * This function sets the value of the variable consumo to the value of the parameter consumo
+     * 
+     * @param consumo the amount of energy consumed by the appliance
+     */
     public void setConsumo(double consumo) {
         this.consumo = consumo;
     }
 
+    /**
+     * This function sets the value of the custo variable to the value of the custo parameter.
+     * 
+     * @param custo The cost of the item.
+     */
     public void setCusto(double custo) {
         this.custo = custo;
     }
 
+    /**
+     * This function sets the value of the variable codigoCasa to the value of the parameter codigoCasa
+     * 
+     * @param codigoCasa The house code.
+     */
     public void setCodigoCasa(int codigoCasa) {
         this.codigoCasa = codigoCasa;
     }
 
+    /**
+     * This function sets the code variable to the value of the code parameter.
+     * 
+     * @param code The HTTP status code.
+     */
     public void setCode(int code) {
         this.code = code;
     }
 
+    /**
+     * It returns a new object with the same attributes as the object it was called on
+     * 
+     * @return A new Fatura object with the same attributes as the original.
+     */
     public Fatura clone() {
         return new Fatura(this.dataInicio, this.dataFim, this.codefornecedor, this.nifCliente, this.consumo, this.custo, this.codigoCasa);
     }
 
+    /**
+     * The compareTo() method returns a negative integer, zero, or a positive integer as this object is
+     * less than, equal to, or greater than the specified object
+     * 
+     * @param fatura The object to be compared.
+     * @return The difference between the dates of the invoices.
+     */
     public int compareTo(Fatura fatura) {
         return this.dataInicio.compareTo(fatura.getDateInicio());
     }
