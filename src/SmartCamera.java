960e904853e4b3f2d76d46907cee9aee7497b41b
sica2602 <sjscosta02@gmail.com>
2022-05-09 16:41:51 +0100

    Adicionada documentação do ficheiro SmartCamera

--- src/SmartCamera.java
+++ src/SmartCamera.java
@@ -1,6 +1,4 @@
-/**
- * SmartCamera is a class that represents a smart camera
- */
+// SmartCamera is a class that represents a smart camera
 public class SmartCamera extends SmartDevices {
 
     private static int counter = 0;
@@ -11,6 +9,7 @@ public class SmartCamera extends SmartDevices {
     private double filesize;
     private float consumoDiarioCamera;
 
+    // The constructor of the class SmartCamera.
     public SmartCamera() {
         this.isON = false;
         this.id = 0;
@@ -20,6 +19,7 @@ public class SmartCamera extends SmartDevices {
         this.consumoDiarioCamera = 0;
     }
 
+    // It's a constructor.
     public SmartCamera(boolean isON, int resolution_x, int resolution_y, double filesize, float consumoDiarioCamera) {
         this.isON = isON;
         this.id = counter++;
@@ -29,58 +29,126 @@ public class SmartCamera extends SmartDevices {
         this.consumoDiarioCamera = consumoDiarioCamera;
     }
 
+    /**
+     * This function returns a boolean value that indicates whether the light is on or off
+     * 
+     * @return isON
+     */
     public boolean isON() {
         return isON;
     }
 
+    /**
+     * This function returns the id of the object.
+     * 
+     * @return The id of the object.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * This function returns the value of the variable resolution_x
+     * 
+     * @return The resolution_x variable is being returned.
+     */
     public int getResolution_x() {
         return resolution_x;
     }
 
+    /**
+     * This function returns the value of the variable resolution_y
+     * 
+     * @return The resolution_y variable is being returned.
+     */
     public int getResolution_y() {
         return resolution_y;
     }
 
+    /**
+     * This function returns the filesize of the file
+     * 
+     * @return The filesize of the file.
+     */
     public double getFilesize() {
         return filesize;
     }
 
+    /**
+     * This function returns the value of the variable consumoDiarioCamera
+     * 
+     * @return The value of the variable consumoDiarioCamera.
+     */
     public double getConsumoDiarioCamera() {
         return consumoDiarioCamera;
     }
 
+    /**
+     * This function sets the value of the isON variable to true.
+     */
     public void setON() {
         isON = true;
     }
 
+    /**
+     * This function sets the value of the isON variable to false.
+     */
     public void setOFF() {
         isON = false;
     }
 
+    /**
+     * This function sets the id of the object to the value of the parameter id.
+     * 
+     * @param id The id of the user.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * This function sets the resolution_x variable to the value of the resolution_x parameter.
+     * 
+     * @param resolution_x The width of the image in pixels
+     */
     public void setResolution_x(int resolution_x) {
         this.resolution_x = resolution_x;
     }
 
+    /**
+     * This function sets the resolution_y variable to the value of the parameter resolution_y
+     * 
+     * @param resolution_y The height of the image in pixels.
+     */
     public void setResolution_y(int resolution_y) {
         this.resolution_y = resolution_y;
     }
 
+    /**
+     * This function sets the filesize of the file
+     * 
+     * @param filesize The size of the file in bytes.
+     */
     public void setFilesize(double filesize) {
         this.filesize = filesize;
     }
 
+    /**
+     * This function sets the value of the variable consumoDiarioCamera to the value of the parameter
+     * consumoDiarioCamera
+     * 
+     * @param consumoDiarioCamera The daily consumption of the camera.
+     */
     public void setConsumoDiarioCamera(float consumoDiarioCamera) {
         this.consumoDiarioCamera = consumoDiarioCamera;
     }
 
+    /**
+     * It compares two objects of the same class.
+     * 
+     * @param o The object to compare with.
+     * @return The hashCode() method is overridden to return the hashCode of the id field.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o)
@@ -93,6 +161,11 @@ public class SmartCamera extends SmartDevices {
                 && consumoDiarioCamera == that.consumoDiarioCamera;
     }
 
+    /**
+     * The toString() method returns a string representation of the object
+     * 
+     * @return The method toString() is being returned.
+     */
     @Override
     public String toString() {
         return "SmartCamera{" +
@@ -105,10 +178,20 @@ public class SmartCamera extends SmartDevices {
                 '}';
     }
 
+    /**
+     * It returns a new object that is a copy of the current object
+     * 
+     * @return A new SmartCamera object with the same attributes as the original.
+     */
     public SmartCamera clone() {
         return new SmartCamera(this.isON, this.resolution_x, this.resolution_y, this.filesize, this.consumoDiarioCamera);
     }
 
+    /**
+     * It returns the daily consumption of the camera, based on the resolution of the camera
+     * 
+     * @return The method returns the daily consumption of the camera.
+     */
     public double consumoDiario() {
         double grauConsumo = 0.00;
         if (this.resolution_x == 640 && this.resolution_y == 480)
@@ -126,5 +209,4 @@ public class SmartCamera extends SmartDevices {
 
         return (grauConsumo * this.consumoDiarioCamera);
     }
-
 }
