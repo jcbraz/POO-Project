bc01f9935c144d12442dd60d5fc9909a9f03ad62
sica2602 <sjscosta02@gmail.com>
2022-05-09 16:49:56 +0100

    Adicionada documentação dos ficheiros SmartSpeaker e Smart devices

--- src/SmartDevices.java
+++ src/SmartDevices.java
@@ -3,69 +3,117 @@ import java.util.Objects;
 
 public abstract class SmartDevices implements Serializable {
 
+    // The attributes of the class SmartDevices
     private static int counter = 0;
     private boolean isON;
     private int id;
     private float custoInstalacao;
 
-    
-
+    // A constructor that initializes the attributes of the class SmartDevices.
     public SmartDevices() {
         this.isON = false;
         this.id = 0;
         this.custoInstalacao = 0;
-
     }
 
+    // A constructor that initializes the attributes of the class SmartDevices.
     public SmartDevices(boolean isON, float custoInstalacao) {
         this.isON = isON;
         this.id = counter++;
         this.custoInstalacao = custoInstalacao;
-
     }
 
+    /**
+     * If this is a SmartBulb, return true, otherwise return false.
+     * 
+     * @return A boolean value.
+     */
     public boolean isBulb() {
         return this instanceof SmartBulb;
     }
 
+    /**
+     * If this object is a SmartSpeaker, return true, otherwise return false.
+     * 
+     * @return A boolean value.
+     */
     public boolean isSpeaker() {
         return this instanceof SmartSpeaker;
     }
 
+    /**
+     * If this object is a SmartCamera, return true, otherwise return false.
+     * 
+     * @return A boolean value.
+     */
     public boolean isCamera() {
         return this instanceof SmartCamera;
     }
 
+    /**
+     * This function returns the value of the isON variable.
+     * 
+     * @return isON
+     */
     public boolean isON() {
         return isON;
     }
 
+    /**
+     * This function returns the id of the object
+     * 
+     * @return The id of the object.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * > This function returns the value of the variable custoInstalacao
+     * 
+     * @return The value of the variable custoInstalacao.
+     */
     public float getCustoInstalacao() {
         return custoInstalacao;
     }
 
+    // This function sets the value of the isON variable to true.
     public void setON() {
         isON = true;
     }
 
+    // This function sets the value of the isON variable to false.
     public void setOFF() {
         isON = false;
     }
 
+    /**
+     * This function sets the id of the object to the value of the parameter id.
+     * 
+     * @param id The id of the user.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
-
-    
+    /**
+     * This function sets the value of the variable custoInstalacao to the value of the parameter
+     * custoInstalacao
+     * 
+     * @param custoInstalacao The cost of installing the equipment.
+     */
     public void setCustoInstalacao(float custoInstalacao) {
         this.custoInstalacao = custoInstalacao;
     }
 
+    /**
+     * The function returns true if the object passed as a parameter is the same as the object that
+     * called the function, or if the object passed as a parameter is not null and is of the same class
+     * as the object that called the function
+     * 
+     * @param o The object to compare with.
+     * @return The hashCode() method is overridden to return the hashCode of the id field.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o)
@@ -77,6 +125,11 @@ public abstract class SmartDevices implements Serializable {
                 && Objects.equals(id, that.id);
     }
 
+    /**
+     * The toString() method returns a string representation of the object
+     * 
+     * @return The method toString() is being returned.
+     */
     @Override
     public String toString() {
         return "SmartDevices{" +
@@ -86,7 +139,17 @@ public abstract class SmartDevices implements Serializable {
                 '}';
     }
 
+    /**
+     * The clone() function is used to create a copy of an object
+     * 
+     * @return A new object of the same type as the object that called the method.
+     */
     public abstract SmartDevices clone();
 
+    /**
+     * Returns the amount of energy consumed by the appliance in one day.
+     * 
+     * @return The method is returning the value of the variable consumoDiario.
+     */
     public abstract double consumoDiario();
 }
--- src/SmartSpeaker.java
+++ src/SmartSpeaker.java
@@ -11,6 +11,7 @@ public class SmartSpeaker extends SmartDevices {
     private String marcaEquipamento;
     private float consumoDiarioSpeaker; // função da marca do equipamento
 
+    // It's a constructor.
     public SmartSpeaker() {
         this.isON = false;
         this.volume = 0;
@@ -20,6 +21,7 @@ public class SmartSpeaker extends SmartDevices {
         this.consumoDiarioSpeaker = 0;
     }
 
+    // It's a constructor.
     public SmartSpeaker(boolean isON, int volume, String nomeRadio, String marcaEquipamento,
             float consumoDiarioSpeaker) {
         this.volume = volume;
@@ -29,58 +31,124 @@ public class SmartSpeaker extends SmartDevices {
         this.consumoDiarioSpeaker = consumoDiarioSpeaker;
     }
 
+    /**
+     * This function returns a boolean value that indicates whether the light is on or off
+     * 
+     * @return isON
+     */
     public boolean isON() {
         return isON;
     }
 
+    /**
+     * This function returns the volume of the box.
+     * 
+     * @return The volume of the box.
+     */
     public int getVolume() {
         return volume;
     }
 
+    /**
+     * This function returns the id of the object
+     * 
+     * @return The id of the object.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * This function returns the name of the radio station
+     * 
+     * @return The name of the radio station.
+     */
     public String getNomeRadio() {
         return nomeRadio;
     }
 
+    /**
+     * It returns the value of the variable marcaEquipamento.
+     * 
+     * @return The brand of the equipment.
+     */
     public String getMarcaEquipamento() {
         return marcaEquipamento;
     }
 
+    /**
+     * This function returns the value of the variable consumoDiarioSpeaker
+     * 
+     * @return The value of the variable consumoDiarioSpeaker.
+     */
     public double getconsumoDiarioSpeaker() {
         return consumoDiarioSpeaker;
     }
 
+    // This function sets the value of the isON variable to true.
     public void setON() {
         isON = true;
     }
 
+    // This function sets the value of the isON variable to false.
     public void setOFF() {
         isON = false;
     }
 
+    /**
+     * This function sets the volume of the TV to the value of the parameter.
+     * 
+     * @param volume The volume of the sound.
+     */
     public void setVolume(int volume) {
         this.volume = volume;
     }
 
+    /**
+     * This function sets the id of the object to the value of the parameter id.
+     * 
+     * @param id The id of the user.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * This function sets the name of the radio
+     * 
+     * @param nomeRadio The name of the radio button.
+     */
     public void setNomeRadio(String nomeRadio) {
         this.nomeRadio = nomeRadio;
     }
 
+    /**
+     * This function sets the value of the variable marcaEquipamento to the value of the parameter
+     * marcaEquipamento
+     * 
+     * @param marcaEquipamento The brand of the equipment.
+     */
     public void setMarcaEquipamento(String marcaEquipamento) {
         this.marcaEquipamento = marcaEquipamento;
     }
 
+    /**
+     * This function sets the value of the variable consumoDiarioSpeaker to the value of the parameter
+     * consumoDiarioSpeaker
+     * 
+     * @param consumoDiarioSpeaker The amount of power consumed by the speaker in a day.
+     */
     public void setconsumoDiarioSpeaker(float consumoDiarioSpeaker) {
         this.consumoDiarioSpeaker = consumoDiarioSpeaker;
     }
 
+    /**
+     * If the object is the same object, return true. If the object is null or of a different class,
+     * return false. If the object is of the same class, compare the fields
+     * 
+     * @param o the object to be compared.
+     * @return The hashCode() method is overridden to return the hashCode of the id field.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o)
@@ -94,6 +162,11 @@ public class SmartSpeaker extends SmartDevices {
                 && marcaEquipamento.equals(that.marcaEquipamento);
     }
 
+    /**
+     * The toString() method returns a string representation of the object
+     * 
+     * @return The toString method is being returned.
+     */
     @Override
     public String toString() {
         return "SmartSpeaker{" +
@@ -106,10 +179,22 @@ public class SmartSpeaker extends SmartDevices {
                 '}';
     }
 
+    /**
+     * The clone() method is used to create a copy of an object
+     * 
+     * @return A new object of the same type as the object that invoked the method, with the same
+     * state.
+     */
     public SmartSpeaker clone() {
         return new SmartSpeaker(isON, volume, nomeRadio, marcaEquipamento, consumoDiarioSpeaker);
     }
 
+    /**
+     * It returns the daily consumption of the speaker, which is calculated by multiplying the daily
+     * consumption of the speaker by a factor that depends on the volume of the speaker
+     * 
+     * @return The return is the consumption of the speaker.
+     */
     public double consumoDiario() {
         double grauConsumo = 0.00;
         if (this.volume >= 25 && this.volume < 50)
@@ -121,5 +206,4 @@ public class SmartSpeaker extends SmartDevices {
 
         return (grauConsumo * this.consumoDiarioSpeaker);
     }
-
 }
