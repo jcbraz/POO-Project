8d925cff92e4be04db0ff3bbe4a4e5e286844bc4
sica2602 <sjscosta02@gmail.com>
2022-05-09 15:29:29 +0100

    Adicionada documentação nos ficheiros Articulador e CasaInteligente

--- src/Articulador.java
+++ src/Articulador.java
@@ -1,52 +1,92 @@
-/**
- * It's a class that represents a system that manages houses, suppliers and bills
- */
 import java.util.Map;
 import java.util.HashMap;
 import java.util.stream.Collectors;
 
 public class Articulador {
 
+    // It's declaring the variables that will be used in the class.
     private Map<Integer, CasaInteligente> casas;
     private Map<Integer, Fornecedor> fornecedores;
     private Map<Integer, Fatura> faturas;
 
+    // It's the constructor of the class Articulador.
     public Articulador() {
         this.casas = new HashMap<Integer, CasaInteligente>();
         this.fornecedores = new HashMap<Integer, Fornecedor>();
         this.faturas = new HashMap<Integer, Fatura>();
     }
 
+    // It's a copy constructor.
     public Articulador(Articulador ci) {
         setCasas(ci.getCasas());
         setFornecedores(ci.getFornecedores());
         setFaturas(ci.getFaturas());
     }
 
+    /**
+     * Return a map of all the houses in the city, where the key is the house's ID and the value is the
+     * house itself.
+     * 
+     * @return A copy of the map.
+     */
     public Map<Integer, CasaInteligente> getCasas() {
         return this.casas.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
+    /**
+     * Return a map of all the entries in the fornecedores map, where the key is the key of the entry
+     * and the value is the value of the entry.
+     * 
+     * @return A map with the key and value of the fornecedores map.
+     */
     public Map<Integer, Fornecedor> getFornecedores() {
         return this.fornecedores.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
+    /**
+     * Return a map of all the invoices, where the key is the invoice id and the value is the invoice
+     * itself.
+     * 
+     * @return A copy of the map.
+     */
     public Map<Integer, Fatura> getFaturas() {
         return this.faturas.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
+    /**
+     * > This function sets the value of the variable `casas` to the value of the parameter `casas`
+     * 
+     * @param casas A map of all the houses in the system. The key is the house's ID.
+     */
     public void setCasas(Map<Integer, CasaInteligente> casas) {
         this.casas = casas;
     }
 
+    /**
+     * It sets the value of the fornecedores attribute to the value of the fornecedores parameter
+     * 
+     * @param fornecedores A map of fornecedores, where the key is the fornecedor's ID and the value is
+     * the fornecedor itself.
+     */
     public void setFornecedores(Map<Integer, Fornecedor> fornecedores) {
         this.fornecedores = fornecedores;
     }
 
+    /**
+     * It sets the value of the faturas attribute to the value of the faturas parameter
+     * 
+     * @param faturas A map of all the invoices that are in the system.
+     */
     public void setFaturas(Map<Integer, Fatura> faturas) {
         this.faturas = faturas;
     }
 
+    /**
+     * The function returns a string that contains the values of the variables casas, fornecedores and
+     * faturas
+     * 
+     * @return The toString method is being returned.
+     */
     public String toString() {
         return "CasaInteligente{" +
                 ", casas=" + casas +
@@ -55,6 +95,13 @@ public class Articulador {
                 '}';
     }
 
+    /**
+     * If the object is the same object, or if the object is not null and is of the same class, then
+     * check if the fields are equal
+     * 
+     * @param o the object to compare to
+     * @return The hashCode of the object.
+     */
     public boolean equals(Object o) {
         if (this == o)
             return true;
@@ -66,30 +113,68 @@ public class Articulador {
                 faturas.equals(that.faturas);
     }
 
+    /**
+     * This function returns a new Articulador object that is a copy of the Articulador object that
+     * called this function.
+     * 
+     * @return A new Articulador object with the same values as the original.
+     */
     public Articulador clone() {
         return new Articulador(this);
     }
 
+    /**
+     * It adds a supplier to the list of suppliers
+     * 
+     * @param fornecedor The fornecedor object to be added to the fornecedores HashMap.
+     */
     public void addFornecedor(Fornecedor fornecedor) {
         this.fornecedores.put(fornecedor.getCode(), fornecedor);
     }
 
+    /**
+     * > This function checks if a supplier exists in the system
+     * 
+     * @param codigoFornecedor The supplier's code.
+     * @return A boolean value.
+     */
     public boolean existeFornecedor(int codigoFornecedor) {
         return this.fornecedores.containsKey(codigoFornecedor);
     }
 
+    /**
+     * It adds a house to the list of houses
+     * 
+     * @param ci CasaInteligente object
+     */
     public void addCasa(CasaInteligente ci) {
         this.casas.put(ci.getCode(), ci);
     }
 
+    /**
+     * > This function returns true if the house with the given code exists in the system
+     * 
+     * @param codigoCasa The code of the house.
+     * @return A boolean value.
+     */
     public boolean existeCasa(int codigoCasa) {
         return this.casas.containsKey(codigoCasa);
     }
 
+    /**
+     * > Removes a house from the list of houses
+     * 
+     * @param code The code of the house to be removed.
+     */
     public void removeCasa(int code) {
         this.casas.remove(code);
     }
 
+    /**
+     * Adds a Fatura to the map of Faturas.
+     * 
+     * @param f Fatura object
+     */
     public void addFaturaToMap(Fatura f) {
         this.faturas.put(f.getCode(), f);
     }
--- src/CasaInteligente.java
+++ src/CasaInteligente.java
@@ -1,7 +1,3 @@
-/**
- * This class represents a smart house, which has a name, an address, a list of smart devices, and a
- * list of bills
- */
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.ArrayList;
@@ -9,6 +5,7 @@ import java.util.HashMap;
 
 public class CasaInteligente {
 
+    // The class attributes.
     private static int counter = 0;
     private String nomeProprietario;
     private int nifProprietario;
@@ -18,6 +15,7 @@ public class CasaInteligente {
                                            // ARTICULADOR (CONVEM GUARDAR COPIA NO FORNECEDOR)
     private Map<String, Map<Integer, SmartDevices>> divisoes;
 
+    // A constructor.
     public CasaInteligente() {
         this.nomeProprietario = "";
         this.nifProprietario = 0;
@@ -27,6 +25,7 @@ public class CasaInteligente {
         this.divisoes = new HashMap<>();
     }
 
+    // A constructor.
     public CasaInteligente(String nomeProprietario, int nifProprietario, String morada) {
         this.nomeProprietario = nomeProprietario;
         this.nifProprietario = nifProprietario;
@@ -36,6 +35,7 @@ public class CasaInteligente {
         this.divisoes = new HashMap<>();
     }
 
+    // A copy constructor.
     public CasaInteligente(CasaInteligente ci) {
         setMorada(ci.getMorada());
         setNomeProprietario(ci.getNomeProprietario());
@@ -45,57 +45,127 @@ public class CasaInteligente {
         setCodigoFaturas(ci.getCodigosDeFaturas());
     }
 
+    /**
+     * This function returns the name of the owner of the Property
+     * 
+     * @return The name of the owner of the Property.
+     */
     public String getNomeProprietario() {
         return this.nomeProprietario;
     }
 
+    /**
+     * > This function returns the nifProprietario of the object
+     * 
+     * @return The nifProprietario
+     */
     public int getNifProprietario() {
         return this.nifProprietario;
     }
 
+    /**
+     * This function returns the value of the variable morada.
+     * 
+     * @return The address of the client.
+     */
     public String getMorada() {
         return this.morada;
     }
 
+    /**
+     * This function returns an ArrayList of Integers that contains the codes of the invoices
+     * 
+     * @return The method returns an ArrayList of Integers.
+     */
     public ArrayList<Integer> getCodigosDeFaturas() {
         return this.codigosDeFaturas;
     }
 
+    /**
+     * > This function returns the code of the current object
+     * 
+     * @return The code of the enum.
+     */
     public int getCode() {
         return this.code;
     }
 
+    /**
+     * It returns a map of the form <String, Map<Integer, SmartDevices>>, where the String is the name
+     * of the division and the Map<Integer, SmartDevices> is a map of the form <Integer, SmartDevices>,
+     * where the Integer is the id of the device and the SmartDevices is the device itself
+     * 
+     * @return A map with the key being the name of the division and the value being a map with the key
+     * being the floor number and the value being a SmartDevices object.
+     */
     public Map<String, Map<Integer, SmartDevices>> getDivisoes() {
         return this.divisoes.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
+    /**
+     * This function sets the name of the owner of the Property
+     * 
+     * @param nomeProprietario The name of the owner of the Property.
+     */
     public void setNomeProprietario(String nomeProprietario) {
         this.nomeProprietario = nomeProprietario;
     }
 
+    /**
+     * This function sets the nifProprietario variable to the value of the nifProprietario parameter
+     * 
+     * @param nifProprietario The NIF of the owner of the Property.
+     */
     public void setNifProprietario(int nifProprietario) {
         this.nifProprietario = nifProprietario;
     }
 
+    /**
+     * This function sets the morada of the object
+     * 
+     * @param morada The address of the client.
+     */
     public void setMorada(String morada) {
         this.morada = morada;
     }
 
+    /**
+     * This function sets the code variable to the value of the code parameter.
+     * 
+     * @param code The HTTP status code.
+     */
     public void setCode(int code) {
         this.code = code;
     }
 
+    /**
+     * It creates a new ArrayList, iterates over the given ArrayList, and adds each element to the new ArrayList
+     * 
+     * @param codigosDeFaturas ArrayList of Integers
+     */
     public void setCodigoFaturas(ArrayList<Integer> codigosDeFaturas) {
         this.codigosDeFaturas = new ArrayList<Integer>();
         codigosDeFaturas.forEach(c -> this.codigosDeFaturas.add(c));
     }
 
+    /**
+     * It takes a map of maps of SmartDevices and copies it to the class's map of maps of SmartDevices
+     * 
+     * @param divisoes Map<String, Map<Integer, SmartDevices>>
+     */
     public void setDivisoes(Map<String, Map<Integer, SmartDevices>> divisoes) {
         this.divisoes = new HashMap<>();
         divisoes.entrySet().stream().forEach(e -> this.divisoes.put(e.getKey(), e.getValue()));
     }
 
 
+    /**
+     * If the object is the same object, return true. If the object is null or of a different class,
+     * return false. If the object is of the same class, compare the fields
+     * 
+     * @param o the object to be compared.
+     * @return The hashCode of the object.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o)
@@ -108,6 +178,11 @@ public class CasaInteligente {
                 && divisoes.equals(that.divisoes);
     }
 
+    /**
+     * The toString() method returns a string representation of the object
+     * 
+     * @return The toString method is being returned.
+     */
     @Override
     public String toString() {
         return "CasaInteligente{" +
@@ -119,37 +194,79 @@ public class CasaInteligente {
                 '}';
     }
 
+    /**
+     * The clone() method creates a new object that is a copy of the original object.
+     * 
+     * @return A new CasaInteligente object with the same state as the original.
+     */
     public CasaInteligente clone() {
         return new CasaInteligente(this);
     }
 
+    /**
+     * It returns true if there's a light bulb with the given key in any of the rooms
+     * 
+     * @param key The key of the bulb to be checked
+     * @return a boolean value.
+     */
     public boolean existeLuz(int key) {
         return this.divisoes.entrySet().stream()
                 .anyMatch(divisao -> divisao.getValue().containsKey(key) && divisao.getValue().get(key).isBulb());
     }
 
+    /**
+     * It returns true if there is a speaker with the given key in the map
+     * 
+     * @param key the key of the speaker
+     * @return A boolean value.
+     */
     public boolean existeSpeaker(int key) {
         return this.divisoes.entrySet().stream()
                 .anyMatch(divisao -> divisao.getValue().containsKey(key) && divisao.getValue().get(key).isSpeaker());
     }
 
+    /**
+     * It returns true if there is a camera with the given key in the map
+     * 
+     * @param key The key of the camera to be checked
+     * @return A boolean value.
+     */
     public boolean existeCamera(int key) {
         return this.divisoes.entrySet().stream()
                 .anyMatch(divisao -> divisao.getValue().containsKey(key) && divisao.getValue().get(key).isCamera());
     }
 
+    /**
+     * It returns true if the map contains a key that matches the parameter
+     * 
+     * @param key The key to be searched for.
+     * @return A boolean value.
+     */
     public boolean existeDivisao(String key) {
         return this.divisoes.containsKey(key);
     }
 
+    /**
+     * It adds a new division to the house
+     * 
+     * @param divisaokey The name of the division.
+     * @param devices A map of devices, where the key is the device's ID and the value is the device
+     * itself.
+     */
     public void addDivisao(String divisaokey, Map<Integer, SmartDevices> devices) {
         this.divisoes.put(divisaokey, devices);
     }
 
+    /**
+     * It removes the division with the given key from the list of divisions
+     * 
+     * @param key The key of the divisao to remove.
+     */
     public void removeDivisao(String key) {
         this.divisoes.remove(key);
     }
 
+    // For each division, if it has a bulb, turn it on.
     public void setAllLuzesON() {
         this.divisoes.entrySet().stream().filter(
                 divisao -> divisao.getValue().entrySet().stream().anyMatch(device -> device.getValue().isBulb()))
@@ -157,6 +274,7 @@ public class CasaInteligente {
                         divisao -> divisao.getValue().entrySet().stream().forEach(device -> device.getValue().setON()));
     }
 
+    // For each division, if it has a bulb, turn it off
     public void setAllLuzesOFF() {
         this.divisoes.entrySet().stream().filter(
                 divisao -> divisao.getValue().entrySet().stream().anyMatch(device -> device.getValue().isBulb()))
@@ -164,6 +282,7 @@ public class CasaInteligente {
                         .forEach(device -> device.getValue().setOFF()));
     }
 
+    // For each division, if it has a speaker, turn it on.
     public void setAllSpeakersON() {
         this.divisoes.entrySet().stream().filter(
                 divisao -> divisao.getValue().entrySet().stream().anyMatch(device -> device.getValue().isSpeaker()))
@@ -171,6 +290,7 @@ public class CasaInteligente {
                         divisao -> divisao.getValue().entrySet().stream().forEach(device -> device.getValue().setON()));
     }
 
+    // For each division, if it has a speaker, turn it off
     public void setAllSpeakersOFF() {
         this.divisoes.entrySet().stream().filter(
                 divisao -> divisao.getValue().entrySet().stream().anyMatch(device -> device.getValue().isSpeaker()))
@@ -178,6 +298,7 @@ public class CasaInteligente {
                         .forEach(device -> device.getValue().setOFF()));
     }
 
+    // It sets all cameras to ON
     public void setAllCamerasON() {
         this.divisoes.entrySet().stream().filter(
                 divisao -> divisao.getValue().entrySet().stream().anyMatch(device -> device.getValue().isCamera()))
@@ -185,6 +306,7 @@ public class CasaInteligente {
                         divisao -> divisao.getValue().entrySet().stream().forEach(device -> device.getValue().setON()));
     }
 
+    // It sets all cameras off
     public void setAllCamerasOFF() {
         this.divisoes.entrySet().stream().filter(
                 divisao -> divisao.getValue().entrySet().stream().anyMatch(device -> device.getValue().isCamera()))
@@ -192,44 +314,85 @@ public class CasaInteligente {
                         .forEach(device -> device.getValue().setOFF()));
     }
 
+    // For each division, for each device, set the device to ON
     public void setAllDevicesON() {
         this.divisoes.entrySet()
                 .forEach(divisao -> divisao.getValue().entrySet().forEach(device -> device.getValue().setON()));
     }
 
+    // For each division, for each device, set the device to OFF
     public void setAllDevicesOFF() {
         this.divisoes.entrySet()
                 .forEach(divisao -> divisao.getValue().entrySet().forEach(device -> device.getValue().setOFF()));
     }
 
+    /**
+     * It checks if a device exists in a division
+     * 
+     * @param divisaokey The key of the division
+     * @param id the id of the device
+     * @return A boolean value.
+     */
     public boolean existeDeviceInDivisao(String divisaokey, int id) {
         return this.divisoes.get(divisaokey).containsKey(id);
     }
 
+    /**
+     * It adds a device to a division
+     * 
+     * @param nomeDivisao The name of the division you want to add the device to.
+     * @param device The device to be added to the division.
+     */
     public void addDeviceToDivisao(String nomeDivisao, SmartDevices device) {
         this.divisoes.get(nomeDivisao).put(device.getId(), device);
     }
 
+    /**
+     * It removes a device from a division
+     * 
+     * @param divisaokey The key of the division you want to remove the device from.
+     * @param id the id of the device
+     */
     public void removeDeviceFromDivisao(String divisaokey, int id) {
         this.divisoes.get(divisaokey).remove(id);
     }
 
+    /**
+     * For each division, if it contains the device with the given id, turn it on.
+     * 
+     * @param id the id of the device you want to turn on
+     */
     public void turnOnSpecificDevice(int id) {
         this.divisoes.entrySet().stream().filter(divisao -> divisao.getValue().containsKey(id))
                 .forEach(divisao -> divisao.getValue().get(id).setON());
     }
 
+    /**
+     * For each division, if the division contains the device with the given id, turn off the device.
+     * 
+     * @param id The id of the device you want to turn off.
+     */
     public void turnOffSpecificDevice(int id) {
         this.divisoes.entrySet().stream().filter(divisao -> divisao.getValue().containsKey(id))
                 .forEach(divisao -> divisao.getValue().get(id).setOFF());
     }
 
+    /**
+     * It sums the daily consumption of all devices in all divisions of the house
+     * 
+     * @return The sum of the daily energy consumption of all devices in the house.
+     */
     public double energiaTotalDiariaCasa() {
         return this.divisoes.values().stream()
                 .mapToDouble(divisao -> divisao.values().stream().mapToDouble(device -> device.consumoDiario()).sum())
                 .sum();
     }
 
+    /**
+     * Adds a new invoice code to the list of invoice codes.
+     * 
+     * @param codeFatura The code of the invoice to be added to the list of invoices.
+     */
     public void addFatura(int codeFatura) {
         this.codigosDeFaturas.add(codeFatura);
     }
